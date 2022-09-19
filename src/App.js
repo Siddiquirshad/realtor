5cb00bbe5f8bb040a9d0482a07ec8170fe936700
siddiqui Irshad <siddiquirshad21@gmail.com.com>
2022-09-19 13:44:07 +0530

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-//import logo from './logo.svg';
-//import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
     <>
-      <h1 className='text-2xl bgv'> Hello World </h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+        </Routes>
+
+      </Router>
     </>
   );
 }
